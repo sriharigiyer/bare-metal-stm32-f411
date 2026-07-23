bd2cc2edeaec1ca81dafa536d3e962fff78f383c
Srihari <srihari.g.iyer@gmail.com>
2026-07-23 10:22:59 +0530

    Add comments to GPIOA_PIN_5_HAL.C

--- Chapter_02_Register_Addresses/GPIOA_PIN_5_HAL.C
+++ Chapter_02_Register_Addresses/GPIOA_PIN_5_HAL.C
@@ -1,6 +1,6 @@
-#include "stm32f4xx_hal.h"
-GPIO_InitTypeDef Gpio_InitStruct = {0};
-_HAL_RCC_GPIO_CLK_ENABLE();
-GPIO_InitStruct.Pin =GPIO_PIN_5;
+#include "stm32f4xx_hal.h" //specific library to STM32F4 series
+GPIO_InitTypeDef Gpio_InitStruct = {0};//declare and initialize the GPIO_InitTypeDef structure  
+_HAL_RCC_GPIO_CLK_ENABLE();//enables clock for GPIO port A
+GPIO_InitStruct.Pin =GPIO_PIN_5;//sets pin to be configures(pin 5 in this case)
 GPIO_InitStruct.Mode =GPIO_MODE_OUTPUT_PP;
 HAL_GPIO_Init(GPIOA, *GPIO_Initstruct);
